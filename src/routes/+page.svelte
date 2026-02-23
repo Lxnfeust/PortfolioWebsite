79deda439f81b4f32a5b79bed9afa94c789a3154
Mattéo Lambert <matteo.lambert@mmibordeaux.com>
2026-02-23 22:09:35 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div class="container_home">
+    <div class="heading">
+        <span>A.</span>
+        <h1>PORTFOLIO</h1>
+    </div>
+    <div class="Section_selectedprojects">
+        <h2><a href="/Projects">Selected Projects</a></h2>
+    </div>
+    <div class="Section_seemore">
+        <h2>Want to see more?</h2>
+        <div class="seemore_items">
+            <span>All Works</span>
+            <span>About Me</span>
+            <span>Playground</span>
+        </div>
+    </div>
+        
+</div>
