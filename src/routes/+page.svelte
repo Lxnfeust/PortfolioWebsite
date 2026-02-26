91ccaefcdc579c3bb60cd2330c6f93caa2f39bdb
favoreat <favoreat@MBP-de-favoreat.lan>
2026-02-26 23:44:49 +0100

    fonts + homepage_section_selected

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,45 @@
 <div class="container_home">
     <div class="heading">
         <span>A.</span>
-        <h1>PORTFOLIO</h1>
+        <h1>Portfolio</h1>
+        <hr>
     </div>
     <div class="Section_selectedprojects">
-        <h2><a href="/Projects">Selected Projects</a></h2>
+        <h2>Selected Projects</h2>
+        <div class="selectedprojects">
+
+            <div class="sp_card">
+                <div class="sp_card_Line">
+                    <div class="sp_card_Item">
+                        <span>Title</span>
+                        <p>Nuit des Musées</p>
+                    </div>
+                    <div class="sp_card_Item">
+                        <span>Rôle</span>
+                        <p>3D</p>
+                    </div>
+                </div>
+                <div class="sp_card_ProjectName">
+                    <span>Nuit des Musées</span>
+                </div>
+                <div class="sp_card_Line">
+                    <div class="sp_card_Item">
+                        <span>Title</span>
+                        <p>2024</p>
+                    </div>
+                    <div class="sp_card_Item">
+                        <span>Domaine</span>
+                        <p>Exp. Interactive</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
     <div class="Section_seemore">
         <h2>Want to see more?</h2>
         <div class="seemore_items">
-            <span>All Works</span>
+            <span><a href="/Projects">All Works</a></span>
             <span>About Me</span>
             <span>Playground</span>
         </div>
---
+++ src/routes/Projects/+page.svelte
@@ -0,0 +1,22 @@
+<div class="container_projects">
+    <div class="heading">
+        <span>B.</span>
+        <h1>Works</h1>
+        <hr>
+    </div>
+    <div class="content_projects">
+        <div class="c_p_cover+index+indic">
+            <div class="c_p_projects">
+                <div class="c_p_projects_cover">
+                    
+                </div>
+                <div class="c_p_projects_index">
+                    
+                </div>
+            </div>
+            <div class="c_p_indic">
+                
+            </div>
+        </div>
+    </div>
+</div>
