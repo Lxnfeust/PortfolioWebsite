3d9d157a9d886bf558186d98067d5225b3b79c4f
favoreat <favoreat@MBP-de-favoreat.lan>
2026-02-28 00:23:14 +0100

    page projects + start project pages

---
+++ src/routes/Project_Page/Pr_NuitDesMusees/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container_projects">
+    <div class="heading">
+        <span>01.</span>
+        <h1>Nuit des Musées</h1>
+        <hr>
+    </div>
+</div>
--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -5,18 +5,204 @@
         <hr>
     </div>
     <div class="content_projects">
-        <div class="c_p_cover+index+indic">
-            <div class="c_p_projects">
-                <div class="c_p_projects_cover">
+        <div class="Section_projects">
+            <div class="projects_cover">
+                <div class="image_cover">
                     
                 </div>
-                <div class="c_p_projects_index">
-                    
+                <div class="text_projects">
+                    <h2>Liste de tous mes projets.</h2>
+                    <p>Voici l’ensemble de mes projets, ils contiennent à la fois des travaux réalisés à l’école, des travaux personnels et des réalisation professionnelles réalisées en entreprise (dans le cadre d’alternances). Restez à l’affût, vous pourriez voir de nouveaux projets bientôt !</p>
                 </div>
             </div>
-            <div class="c_p_indic">
-                
+            <div class="projects_index">
+
+                <a href="/Project_Page/Pr_NuitDesMusees">
+                    <div class="projects_item">
+                        <div class="project_item_number">
+                            <span>01.</span>
+                        </div>
+                        <div class="project_item_info">
+                            <div class="project_item_category">
+                                <span>Titre</span>
+                                <h3>Nuit des Musées</h3>
+                            </div>
+                            <div class="project_item_category">
+                                <span>Année</span>
+                                <h3>2024</h3>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>02.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>A.A.L.C.C</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2025</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>03.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Série Motion</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2026</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>04.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Château Lagrange</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2025</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>05.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>SHowreel</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2024</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>06.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Bacchanight</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2023</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>07.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Valeria</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2024</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>08.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Sapio Spirits</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2026</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>09.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Domaine Giscours</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2026</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>10.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>La Boîte à Cailloux</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2024</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="projects_item">
+                    <div class="project_item_number">
+                        <span>11.</span>
+                    </div>
+                    <div class="project_item_info">
+                        <div class="project_item_category">
+                            <span>Titre</span>
+                            <h3>Bikers au Grand Coeur</h3>
+                        </div>
+                        <div class="project_item_category">
+                            <span>Année</span>
+                            <h3>2024</h3>
+                        </div>
+                    </div>
+                </div>
+
             </div>
+        </div>
+        <div class="Section_indic">
+            <div class="scroll_indic">
+                <hr>
+                <span>scroll</span>
+            </div>
+            <h2>Index</h2>
         </div>
     </div>
 </div>
