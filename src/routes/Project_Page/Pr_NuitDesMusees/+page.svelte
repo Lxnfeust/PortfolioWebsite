4b642595feaf24cfee38a5ba1866cb876b1d58b0
favoreat <favoreat@pc-51.home>
2026-02-28 23:56:00 +0100

    project_page intro section

--- src/routes/Project_Page/Pr_NuitDesMusees/+page.svelte
+++ src/routes/Project_Page/Pr_NuitDesMusees/+page.svelte
@@ -1,7 +1,38 @@
-<div class="container_projects">
+<div class="container_project_page">
     <div class="heading">
         <span>01.</span>
         <h1>Nuit des Musées</h1>
         <hr>
+    </div>
+    <div class="content_project_page">
+        <div class="section_intro">
+            <div class="text_context">
+                <h3>La Nuit Européenne du Musba à Bordeaux.</h3>
+                <p>Le but de ce projet était de mettre à disposition et garantir la maintenance d’une tablette interactive tactile au Musée des Beaux-Arts de Bordeaux. Sur cette même tablette, nous avons créé de multiples expériences en lien avec des œuvres exposées. Pour la partie me concernant, j’ai proposé à mon groupe de travailler sur une expérience web 3D qui se base sur des “maps” de reliefs et reflets qui simulent le déplacement d’une source de lumière comme si elle provenait du tableau original.</p>
+            </div>
+            <div class="year">
+                <h2>2024</h2>
+            </div>
+            <div class="indic">
+                <span>scroll</span>
+                <hr>
+            </div>
+        </div>
+        <div class="section_project">
+            <span>a</span>
+        </div>
+        <div class="section_nextproject">
+            <div class="name">
+                <span>02.</span>
+                <span>→</span>
+            </div>
+            <div class="image">
+
+            </div>
+            <div class="cta">
+                <span>Projet suivant</span>
+                <span>→</span>
+            </div>
+        </div>
     </div>
 </div>
