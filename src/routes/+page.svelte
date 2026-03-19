26eae3b00ef44bb3c37132bf81277a1136f7a8a0
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2026-03-19 01:24:52 +0100

    feat: add projects animations for homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,11 @@
 					src: '/images/la_nuit_des_musees.jpg',
 					alt: 'Nuit des Musées - Image 2'
 				}
-			]
+			],
+			link: {
+				url: '/projects/la-nuit-des-musees',
+				target: ''
+			}
 		},
 		{
 			title: 'AALCC',
@@ -30,7 +34,11 @@
 					src: '/images/aalcc.jpg',
 					alt: 'Nuit des Musées - Image 2'
 				}
-			]
+			],
+			link: {
+				url: '/projects/aalcc',
+				target: ''
+			}
 		},
 		{
 			title: 'Visual series',
@@ -46,7 +54,11 @@
 					src: '/images/serie_motion.jpg',
 					alt: 'Nuit des Musées - Image 2'
 				}
-			]
+			],
+			link: {
+				url: '/projects/visual-series',
+				target: ''
+			}
 		},
 		{
 			title: 'Bacchanight',
@@ -62,7 +74,11 @@
 					src: '/images/bacchanight.jpg',
 					alt: 'Nuit des Musées - Image 2'
 				}
-			]
+			],
+			link: {
+				url: '/projects/bacchanight',
+				target: ''
+			}
 		}
 	];
 
@@ -75,8 +91,106 @@
 			src: string;
 			alt: string;
 		}[];
+		link?: {
+			url: string;
+			target: string;
+		};
 	}
 </script>
+
+{#snippet project(project)}
+	{#if project.link}
+		<a
+			href={project.link.url}
+			target={project.link.target}
+			class="group relative flex w-full justify-center bg-theme-white text-theme-black transition duration-700 hover:bg-theme-black hover:text-theme-white"
+		>
+			<div class="flex w-160 flex-col gap-8 p-4">
+				<div>
+					<div class="flex justify-between">
+						<p class="leading-[105%] uppercase">Title</p>
+						<p class="leading-[105%] uppercase">Role</p>
+					</div>
+					<div class="flex justify-between">
+						<p class="text-xl leading-none font-medium uppercase">{project.title}</p>
+						<p class="text-xl leading-none font-medium uppercase">{project.role}</p>
+					</div>
+				</div>
+				<div class="flex h-50 items-center justify-center gap-6 xl:gap-8">
+					<h3
+						class="font-amiri text-5xl leading-none font-medium uppercase italic opacity-0 transition duration-700 group-hover:opacity-100"
+					>
+						{project.title}
+					</h3>
+					<img
+						src={project.images[0].src}
+						alt={project.images[0].alt}
+						class="group absolute left-[calc(50%-130px)] h-50 max-w-57.5 min-w-57.5 origin-left -translate-x-1/2 rounded object-cover transition-all duration-700 group-hover:left-4 group-hover:translate-0 group-hover:scale-150"
+					/>
+					<img
+						src={project.images[1].src}
+						alt={project.images[1].alt}
+						class="absolute right-[calc(50%-130px)] h-50 max-w-57.5 min-w-57.5 origin-right translate-x-1/2 rounded object-cover transition-all duration-700 group-hover:right-4 group-hover:translate-0 group-hover:scale-150"
+					/>
+				</div>
+				<div>
+					<div class="flex justify-between">
+						<p class="leading-[105%] uppercase">Year</p>
+						<p class="leading-[105%] uppercase">Domain</p>
+					</div>
+					<div class="flex justify-between">
+						<p class="text-2xl leading-[105%] font-medium uppercase">{project.year}</p>
+						<p class="text-2xl leading-[105%] font-medium uppercase">{project.domain}</p>
+					</div>
+				</div>
+			</div>
+		</a>
+	{:else}
+		<div
+			class="flex w-full justify-center bg-theme-white text-theme-black transition duration-700 hover:bg-theme-black hover:text-theme-white"
+		>
+			<div class="group flex w-160 flex-col gap-8 p-4">
+				<div>
+					<div class="flex justify-between">
+						<p class="leading-[105%] uppercase">Title</p>
+						<p class="leading-[105%] uppercase">Role</p>
+					</div>
+					<div class="flex justify-between">
+						<p class="text-xl leading-none font-medium uppercase">{project.title}</p>
+						<p class="text-xl leading-none font-medium uppercase">{project.role}</p>
+					</div>
+				</div>
+				<div class="relative flex h-50 items-center justify-center gap-6 xl:gap-8">
+					<h3
+						class="font-amiri text-5xl leading-none font-medium uppercase italic opacity-0 transition duration-700 group-hover:opacity-100"
+					>
+						{project.title}
+					</h3>
+					<img
+						src={project.images[0].src}
+						alt={project.images[0].alt}
+						class="absolute top-0 left-16 h-50 max-w-57.5 min-w-57.5 rounded object-cover transition-all duration-700 group-hover:-left-full"
+					/>
+					<img
+						src={project.images[1].src}
+						alt={project.images[1].alt}
+						class="absolute top-0 right-16 h-50 max-w-57.5 min-w-57.5 rounded object-cover transition-all duration-700 group-hover:right-full"
+					/>
+				</div>
+				<div>
+					<div class="flex justify-between">
+						<p class="leading-[105%] uppercase">Year</p>
+						<p class="leading-[105%] uppercase">Domain</p>
+					</div>
+					<div class="flex justify-between">
+						<p class="text-2xl leading-[105%] font-medium uppercase">{project.year}</p>
+						<p class="text-2xl leading-[105%] font-medium uppercase">{project.domain}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	{/if}
+{/snippet}
 
 <div class="container_home px-6 pb-6">
 	<div class="flex h-[calc(100lvh-100px)] flex-col justify-between">
@@ -113,42 +227,9 @@
 				>Voir tous les projets →</a
 			>
 		</div>
-		<div class="flex flex-col gap-12 px-25">
-			{#each projects as project, i (i)}
-				<div class="flex flex-col gap-8 p-4">
-					<div>
-						<div class="flex justify-between">
-							<p class="leading-[105%] uppercase">Title</p>
-							<p class="leading-[105%] uppercase">Role</p>
-						</div>
-						<div class="flex justify-between">
-							<p class="text-xl leading-none font-medium uppercase">{project.title}</p>
-							<p class="text-xl leading-none font-medium uppercase">{project.role}</p>
-						</div>
-					</div>
-					<div class="flex justify-center gap-6">
-						<img
-							src={project.images[0].src}
-							alt={project.images[0].alt}
-							class="h-50 max-w-57.5 min-w-57.5 rounded object-cover"
-						/>
-						<img
-							src={project.images[1].src}
-							alt={project.images[1].alt}
-							class="h-50 max-w-57.5 min-w-57.5 rounded object-cover"
-						/>
-					</div>
-					<div>
-						<div class="flex justify-between">
-							<p class="leading-[105%] uppercase">Year</p>
-							<p class="leading-[105%] uppercase">Domain</p>
-						</div>
-						<div class="flex justify-between">
-							<p class="text-2xl leading-[105%] font-medium uppercase">{project.year}</p>
-							<p class="text-2xl leading-[105%] font-medium uppercase">{project.domain}</p>
-						</div>
-					</div>
-				</div>
+		<div class="flex flex-col items-center gap-12">
+			{#each projects as proj, i (i)}
+				{@render project(proj)}
 			{/each}
 		</div>
 	</div>
