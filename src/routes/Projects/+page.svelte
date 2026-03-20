61fb2b7d1726afc2166771029b6e97a6aa4f00cf
Maxime Lust <maxime@favoreatdesign.com>
2026-03-20 19:49:55 +0100

    fix: update projects links in projects page

--- src/routes/Projects/+page.svelte
+++ src/routes/Projects/+page.svelte
@@ -5,99 +5,105 @@
 		{
 			title: 'Nuit des Musées',
 			year: 2024,
-			link: '$lib/data/projects/nuit-des-musees',
+			link: '/projects/nuit-des-musees',
 			image: '/images/nuit_des_musees.jpg'
 		},
 		{
 			title: 'A.A.L.C.C',
 			year: 2025,
-			link: '$lib/data/projects/aalcc',
+			link: '/projects/aalcc',
 			image: '/images/aalcc.jpg'
 		},
 		{
 			title: 'Série Motion',
 			year: 2026,
-			link: '$lib/data/projects/serie-motion',
+			link: '/projects/serie-motion',
 			image: '/images/serie_motion.jpg'
 		},
 		{
 			title: 'Château Lagrange',
 			year: 2025,
-			link: '$lib/data/projects/chateau-lagrange',
+			link: '/projects/chateau-lagrange',
 			image: '/images/chateau_lagrange.jpg'
 		},
 		{
 			title: 'Showreel',
 			year: 2024,
-			link: '$lib/data/projects/showreel',
+			link: '/projects/showreel',
 			image: '/images/showreel.jpg'
 		},
 		{
 			title: 'Bacchanight',
 			year: 2023,
-			link: '$lib/data/projects/bacchanight',
+			link: '/projects/bacchanight',
 			image: '/images/bacchanight.jpg'
 		},
 		{
 			title: 'Valeria',
 			year: 2024,
-			link: '$lib/data/projects/valeria',
+			link: '/projects/valeria',
 			image: '/images/valeria.jpg'
 		},
 		{
 			title: 'Sapio Spirits',
 			year: 2026,
-			link: '$lib/data/projects/sapio-spirits',
+			link: '/projects/sapio-spirits',
 			image: '/images/sapio_spirits.jpg'
 		},
 		{
 			title: 'Domaine Giscours',
 			year: 2026,
-			link: '$lib/data/projects/domaine-giscours',
+			link: '/projects/domaine-giscours',
 			image: '/images/domaine_giscours.jpg'
 		},
 		{
 			title: 'La Boîte à Cailloux',
 			year: 2024,
-			link: '$lib/data/projects/la-boite-a-cailloux',
+			link: '/projects/la-boite-a-cailloux',
 			image: '/images/la_boite_a_cailloux.jpg'
 		},
 		{
 			title: 'Bikers au Grand Coeur',
 			year: 2024,
-			link: '$lib/data/projects/bikers-au-grand-coeur',
+			link: '/projects/bikers-au-grand-coeur',
 			image: '/images/bikers_au_grand_coeur.jpg'
 		}
 	];
 </script>
 
-<div class="flex flex-1 h-full min-h-0 flex-col gap-6 overflow-hidden mx-8">
-	<div class="flex items-baseline gap-4 sticky pointer-events-none">
-		<span class="flex font-metal text-[1.25rem] md:text-[2rem] xl:text-[2.5rem] leading-[85%] uppercase">B.</span>
-		<h1 class="flex font-amiri text-[2.5rem] md:text-[4rem] xl:text-[10rem] -mt-6 -mb-9 uppercase">Projets</h1>
-		<hr class="flex-1 min-w-px h-px bg-black" />
+<div class="mx-8 flex h-full min-h-0 flex-1 flex-col gap-6 overflow-hidden">
+	<div class="pointer-events-none sticky flex items-baseline gap-4">
+		<span
+			class="flex font-metal text-[1.25rem] leading-[85%] uppercase md:text-[2rem] xl:text-[2.5rem]"
+			>B.</span
+		>
+		<h1 class="-mt-6 -mb-9 flex font-amiri text-[2.5rem] uppercase md:text-[4rem] xl:text-[10rem]">
+			Projets
+		</h1>
+		<hr class="h-px min-w-px flex-1 bg-black" />
 	</div>
 
-	<div class="w-full max-h-full flex flex-1 min-h-0 justify-between">
-
+	<div class="flex max-h-full min-h-0 w-full flex-1 justify-between">
 		<!-- Section Projects -->
-		<div class="w-[100rem] flex flex-1 min-h-0 justify-between max-h-full overflow-auto [&::-webkit-scrollbar]:hidden">
-
+		<div
+			class="flex max-h-full min-h-0 w-[100rem] flex-1 justify-between overflow-auto [&::-webkit-scrollbar]:hidden"
+		>
 			<!-- Cover -->
-			<div class="flex flex-1 min-h-0 h-full flex-col justify-between sticky top-0 pb-8">
-				<div class="relative w-[600px] h-[520px] m-auto bg-theme-black">
+			<div class="sticky top-0 flex h-full min-h-0 flex-1 flex-col justify-between pb-8">
+				<div class="relative m-auto h-[520px] w-[600px] bg-theme-black">
 					<img src={imageCover} alt="" class="absolute inset-0 h-full w-full" />
 				</div>
-				<div class="flex flex-col gap-4 w-[40vw]">
-					<h2 class="font-amiri uppercase leading-[85%] text-[3rem]">Liste de tous mes projets.</h2>
-					<p class="font-bricolage-grotesque font-medium leading-[100%] text-[1rem]">
-						Voici l'ensemble de mes projets, réalisés en alternance et scolaires. Restez à l'affût, vous pourriez voir de nouveaux projets bientôt !
+				<div class="flex w-[40vw] flex-col gap-4">
+					<h2 class="font-amiri text-[3rem] leading-[85%] uppercase">Liste de tous mes projets.</h2>
+					<p class="font-bricolage-grotesque text-[1rem] leading-[100%] font-medium">
+						Voici l'ensemble de mes projets, réalisés en alternance et scolaires. Restez à l'affût,
+						vous pourriez voir de nouveaux projets bientôt !
 					</p>
 				</div>
 			</div>
 
 			<!-- Index -->
-			<div class="z-20 flex flex-1 flex-col gap-20 max-h-full relative">
+			<div class="relative z-20 flex max-h-full flex-1 flex-col gap-20">
 				{#each projects as project, i (i)}
 					<a
 						onmouseenter={() => (imageCover = project.image)}
@@ -105,18 +111,28 @@
 						data-image={project.image}
 						class="w-fit"
 					>
-						<div class="flex gap-9 items-end text-theme-black opacity-60 w-fit hover:opacity-100">
-							<div class="font-bricolage-grotesque uppercase leading-[80%] text-[2rem] flex flex-row justify-end items-end">
+						<div class="flex w-fit items-end gap-9 text-theme-black opacity-60 hover:opacity-100">
+							<div
+								class="font-bricolage-grotesque flex flex-row items-end justify-end text-[2rem] leading-[80%] uppercase"
+							>
 								<span>{i > 8 ? i + 1 : '0' + (i + 1)}.</span>
 							</div>
 							<div class="flex gap-4">
-								<div class="flex gap-1 items-start">
-									<span class="font-bricolage-grotesque uppercase leading-[80%] text-[1rem]">Titre</span>
-									<h3 class="font-bricolage-grotesque uppercase leading-[80%] text-[2rem]">{project.title}</h3>
+								<div class="flex items-start gap-1">
+									<span class="font-bricolage-grotesque text-[1rem] leading-[80%] uppercase"
+										>Titre</span
+									>
+									<h3 class="font-bricolage-grotesque text-[2rem] leading-[80%] uppercase">
+										{project.title}
+									</h3>
 								</div>
-								<div class="flex gap-1 items-start">
-									<span class="font-bricolage-grotesque uppercase leading-[80%] text-[1rem]">Année</span>
-									<h3 class="font-bricolage-grotesque uppercase leading-[80%] text-[2rem]">{project.year}</h3>
+								<div class="flex items-start gap-1">
+									<span class="font-bricolage-grotesque text-[1rem] leading-[80%] uppercase"
+										>Année</span
+									>
+									<h3 class="font-bricolage-grotesque text-[2rem] leading-[80%] uppercase">
+										{project.year}
+									</h3>
 								</div>
 							</div>
 						</div>
@@ -127,13 +143,16 @@
 		</div>
 
 		<!-- Section Indic -->
-		<div class="flex flex-col min-h-0 justify-end mt-auto gap-[20vh] sticky bottom-8">
-			<div class="translate-x-[calc(50%-11px)] -rotate-90 flex items-center justify-end gap-4 flex-1 bottom-0">
-				<hr class="w-full min-w-[50px] h-[2px] bg-theme-black" />
-				<span class="font-bricolage-grotesque uppercase font-medium leading-[110%] text-[1.25rem]">scroll</span>
+		<div class="sticky bottom-8 mt-auto flex min-h-0 flex-col justify-end gap-[20vh]">
+			<div
+				class="bottom-0 flex flex-1 translate-x-[calc(50%-11px)] -rotate-90 items-center justify-end gap-4"
+			>
+				<hr class="h-[2px] w-full min-w-[50px] bg-theme-black" />
+				<span class="font-bricolage-grotesque text-[1.25rem] leading-[110%] font-medium uppercase"
+					>scroll</span
+				>
 			</div>
-			<h2 class="font-amiri uppercase text-[3rem] -mb-6">Index</h2>
+			<h2 class="-mb-6 font-amiri text-[3rem] uppercase">Index</h2>
 		</div>
-
 	</div>
 </div>
